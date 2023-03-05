abad9ee4e6f03eceba6bf0e93330cb040aae2a8d
David Davis <david.b.davis@live.com>
2023-03-05 14:43:03 -0600

    add files

--- MidiCtrl/main.m
+++ MidiCtrl/main.m
@@ -6,11 +6,20 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "MIDIWrapper.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+        MIDIWrapper *midi = [[MIDIWrapper alloc] initWithClientName:@"Client" inPort:@"Input Port" outPort:@"Output Port"];
+        
+        NSLog(@"%@", [midi getDeviceList]);
+        
+        MIDIDeviceRef launchpadX = [midi getDevice:@"Launchpad X" ];
+        NSLog(@"%@", [midi getInformationAboutDevice: launchpadX]);
+        
+        NSArray *command = [NSArray arrayWithObjects:[NSNumber numberWithUnsignedInt:0x90], [NSNumber numberWithUnsignedInt:0x0B], [NSNumber numberWithUnsignedInt:0x05],nil];
+        
+        [midi sendData: command withDevice: launchpadX entityIndex:1];
     }
     return 0;
 }
