b48b3339c49ce83a8571230b491cd8218c099453
David Davis <david.b.davis@live.com>
2023-03-05 14:27:35 -0600

    Initial Commit

---
+++ MidiCtrl/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  MidiCtrl
+//
+//  Created by David Davis on 3/5/23.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
